33a71d9efb55eb9938184b303fdc3d05b71941d7
adore <125859102+adorexx@users.noreply.github.com>
2023-02-21 14:20:48 +0800

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,97 @@
+package demo1;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+
+public class Emp implements Serializable {
+    private Integer empno;
+    private String ename;
+    private String job;
+    private String mgr;
+    private Date hiredate;
+    private Integer sal;
+    private Integer deptno;
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "empno=" + empno +
+                ", ename='" + ename + '\'' +
+                ", job='" + job + '\'' +
+                ", mgr='" + mgr + '\'' +
+                ", hiredate=" + hiredate +
+                ", sal=" + sal +
+                ", deptno=" + deptno +
+                '}';
+    }
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(String mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Integer getSal() {
+        return sal;
+    }
+
+    public void setSal(Integer sal) {
+        this.sal = sal;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public Emp() {
+    }
+
+    public Emp(Integer empno, String ename, String job, String mgr, Date hiredate, Integer sal, Integer deptno) {
+        this.empno = empno;
+        this.ename = ename;
+        this.job = job;
+        this.mgr = mgr;
+        this.hiredate = hiredate;
+        this.sal = sal;
+        this.deptno = deptno;
+    }
+}
